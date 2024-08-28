341aba0ea7af091f3d4f78bb6206bd10130e6b19
eoskandlv <eoskandlv@gamil.com>
2024-08-29 01:40:16 +0900

    feat : nav 초기세팅

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -7,6 +7,7 @@
 ┣ @내역: 이수정, 2024-08-29, 최초등록
 ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 */
+
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import "./Nav.scss"
 import { FaPenNib } from "react-icons/fa";
@@ -17,53 +18,53 @@ import { FaBookQuran } from "react-icons/fa6";
 
 const HomeBar = () => {
     return (
-      <div className="homebar">
-        <div className="homebar-wrap">
-          <Link to={"/"} style={{ textDecoration: "none" }}>
-            <div className="homebar-wrap__logo">JOARA</div>
-          </Link>
-          <Link to={"/author"} style={{ textDecoration: "none" }}>
-            <div className="homebar-wrap__item">
-              <div className="homebar-wrap__menu">
-                <div className="homebar-wrap__icon">
-                  <FaPenNib className="icon" IoIosClose size="20" />
-                  Author
-                </div>
-              </div>
+        <div className="homebar">
+            <div className="homebar-wrap">
+                <Link to={"/"} style={{ textDecoration: "none" }}>
+                    <div className="homebar-wrap__logo">JOARA</div>
+                </Link>
+                <Link to={"/author"} style={{ textDecoration: "none" }}>
+                    <div className="homebar-wrap__item">
+                        <div className="homebar-wrap__menu">
+                            <div className="homebar-wrap__icon">
+                                <FaPenNib className="icon" IoIosClose size="20" />
+                                Author
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+                <Link to={"/novel"} style={{ textDecoration: "none" }}>
+                    <div className="homebar-wrap__item">
+                        <div className="homebar-wrap__menu">
+                            <div className="homebar-wrap__icon">
+                                <FaBookTanakh className="icon" IoIosClose size="20" />
+                                Novel
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+                <Link to={"/comix"} style={{ textDecoration: "none" }}>
+                    <div className="homebar-wrap__item">
+                        <div className="homebar-wrap__menu">
+                            <div className="homebar-wrap__icon">
+                                <FaBookQuran className="icon" IoIosClose size="20" />
+                                Comix
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+                <Link to={"/event"} style={{ textDecoration: "none" }}>
+                    <div className="homebar-wrap__item">
+                        <div className="homebar-wrap__menu">
+                            <div className="homebar-wrap__icon">
+                                <BsCalendar2HeartFill className="icon" IoIosClose size="20" />
+                                Event
+                            </div>
+                        </div>
+                    </div>
+                </Link>
             </div>
-          </Link>
-          <Link to={"/novel"} style={{ textDecoration: "none" }}>
-            <div className="homebar-wrap__item">
-              <div className="homebar-wrap__menu">
-                <div className="homebar-wrap__icon">
-                  <FaBookTanakh className="icon" IoIosClose size="20" />
-                  Novel
-                </div>
-              </div>
-            </div>
-          </Link>
-          <Link to={"/comix"} style={{ textDecoration: "none" }}>
-            <div className="homebar-wrap__item">
-              <div className="homebar-wrap__menu">
-                <div className="homebar-wrap__icon">
-                  <FaBookQuran className="icon" IoIosClose size="20" />
-                  Comix
-                </div>
-              </div>
-            </div>
-          </Link>
-          <Link to={"/event"} style={{ textDecoration: "none" }}>
-            <div className="homebar-wrap__item">
-              <div className="homebar-wrap__menu">
-                <div className="homebar-wrap__icon">
-                  <BsCalendar2HeartFill className="icon" IoIosClose size="20" />
-                  Event
-                </div>
-              </div>
-            </div>
-          </Link>
         </div>
-      </div>
     );
 };
 
