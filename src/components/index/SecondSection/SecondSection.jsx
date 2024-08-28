fab7a88de59bb3f5d6364906026327e9b27c4cf3
eoskandlv <eoskandlv@gamil.com>
2024-08-29 06:04:10 +0900

    style index 이미지 수정

--- src/components/index/SecondSection/SecondSection.jsx
+++ src/components/index/SecondSection/SecondSection.jsx
@@ -46,7 +46,8 @@ const SecondSection = () => {
               <div className="left-wrap__text">
                 <h1>뷰어 TTS, 텍스트 음성 변환</h1>
                 <p>
-                  작가, 독자의 보이스를 중심으로 필요한 변화를 선별하여 완전하게 새 단장 했습니다.
+                  작가·독자의 보이스를 중심으로 필요한 변화를 선별하여 완전하게
+                  새 단장 했습니다.
                 </p>
               </div>
             </div>
@@ -65,18 +66,20 @@ const SecondSection = () => {
           <div className="second-index__content__left">
             <div className="left-wrap">
               <div className="left-wrap__text">
-                <h1>수요응답형 대중교통 DRT</h1>
+                <h1>새로워진 사용 친화적 업데이트</h1>
                 <p>
-                  대중교통의 효율을 극대화 한 모빌리티 플랫폼으로 지역 특성과
-                  환경을 고려한 맞춤형 시스템입니다.
+                  홈페이지·아이템샵 오픈, 프리미엄 선물함과 무료 이벤트, 작품
+                  상세 페이지 개선
+                  <br />
+                  그리고 더욱 스마트해진 뷰어로 편하게 조아라를 이용하세요.
                 </p>
               </div>
             </div>
           </div>
           <div className="second-index__content__right">
-            <img src="/images/app/app_01.png" />
-            <img src="/images/app/app_02.png" />
-            <img src="/images/app/app_03.png" />
+            <img src="/images/app/update_01.png" />
+            <img src="/images/app/update_02.png" />
+            <img src="/images/app/update_03.png" />
           </div>
         </div>
         <div className="second-index__text">
